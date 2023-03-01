4e19f55b0250c8551b3c864fa0f93020efe2ced6
Vinicius Queiroz <vinicius13bn@gmail.com>
2023-02-28 22:20:01 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,46 @@
+let lista = []
+
+function add() {
+    let res = document.getElementById('res')
+    let numero = document.getElementById('numero')
+    let n = Number(numero.value)
+    
+    if(n < 1 || n > 100 || lista.includes(n)){
+        alert('Número inválido ou ja listado.')
+    }
+    else {
+        lista.push(n)
+        let item = document.createElement('option')
+        item.text = `Valor ${n} adicionado.`
+        res.appendChild(item)
+    }
+    numero.value = ''
+    numero.focus()
+}
+
+function finalizar() {
+    let resultado = document.querySelector('article#resultado')
+    lista.sort()
+
+    if(lista.length == 0) {
+        alert('Adicione valores antes de finalizar.')
+    }
+    else {
+        let maior = lista[lista.length - 1]
+        let menor = lista[0]
+        let soma = 0
+
+        for(let i = 0; i < lista.length; i++) {
+            soma += lista[i]
+        }
+
+        let media = soma / lista.length
+
+        resultado.innerHTML = ''
+        resultado.innerHTML += `<p>Ao todo temos ${lista.length} elementos.</p>`
+        resultado.innerHTML += `<p>O maior valor foi ${maior}.</p>`
+        resultado.innerHTML += `<p>O menor valor foi ${menor}.</p>`
+        resultado.innerHTML += `<p>A soma de todos os número adicionados é ${soma}.</p>`
+        resultado.innerHTML += `<p>A média de todos os número adicionados é ${media.toFixed(1)}.</p>`
+    }
+}
